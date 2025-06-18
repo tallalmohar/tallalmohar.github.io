f478ecc01c43f19e08d15931349d0ea472190df2
tallalmohar <tallalmoohar@gmail.com>
2025-06-17 23:26:03 -0700

    used email.js for contact me page might add a picture later if i feel frisky with it, otherwise contact page looking and working well

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 import React from "react";
-
+import ContactBox from "../Components/ContactBox";
 
 export default function Contact(){
 	return (
 	<>
 	<div className="container mx-auto px-4 py-8 max-w-5xl">
-		<div>
+		<div className="m-4">
 			<h1 className="text-5xl font-bold">Contact</h1>
 		</div>
+
+		<ContactBox></ContactBox>
+
 	</div>
 	</>
 	)
