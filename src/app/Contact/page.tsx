91d65184584e96d766be5cc9a16772f6651c9c5f
tallalmohar <tallalmoohar@gmail.com>
2025-06-18 12:45:08 -0700

    made the contact page more aesthetic added some margin and padding to make the text centered

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -6,12 +6,12 @@ export default function Contact(){
 	return (
 	<>
 	<div className="container mx-auto px-4 py-8 max-w-5xl">
-		<div className="m-4">
+		<div className="m-4 text-center">
 			<h1 className="text-5xl font-bold">Contact</h1>
 		</div>
 
 		<ContactBox></ContactBox>
-
+		
 	</div>
 	</>
 	)
